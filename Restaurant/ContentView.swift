049ba28ef9c7c9b45d652738da73561779ec29bc
Denis Denisov <denis.denisov8701@gmail.com>
2025-07-03 16:33:40 +0200

    Initial Commit

---
+++ Restaurant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by Denis Denisov on 3/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
